52bd77a9e772055b0d51a46ac82c6e484e81f94e
iozberk <iozberks@gmail.com>
2022-08-26 18:00:59 +0300

    pytest

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+import pathlib
 
+BASE_DIR = pathlib.Path(__file__).parent
 app = FastAPI()
+templates = Jinja2Templates(directory=str(BASE_DIR / "./templates"))
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
 
 
 
