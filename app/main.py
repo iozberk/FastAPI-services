b64dcfd29639b3199b9cd41cf1843005d7bc4d08
iozberk <iozberks@gmail.com>
2022-08-27 16:26:41 +0300

    Last commit (i hope) :(

--- app/main.py
+++ app/main.py
@@ -8,12 +8,13 @@ from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseSettings
 from PIL import Image
+from typing import Optional
 
 class Settings(BaseSettings):
     app_auth_token: str
     debug: bool = False
     echo_active: bool = False
-    app_auth_token_prod: str = None
+    app_auth_token_prod: Optional[str] = None
     skip_auth: bool = False
 
     class Config:
